0a1302e6d8ac4b687d44509de14b3ee5e0cdfc28
T107 <T107@PC257>
2014-06-10 20:52:06 -0500

    Se agrega WebApp y WebAppConfig

---
+++ mavenspring/src/main/java/com/castillo/web/WebAppConfig.java
@@ -0,0 +1,16 @@
+package com.castillo.web;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+
+@EnableWebMvc
+@Configuration
+@ComponentScan("com.castillo.web")
+public class WebAppConfig extends WebMvcConfigurerAdapter{
+    
+    
+    
+}
